052f14161cd71f8e157a7c1be6c56d9d3ed015fd
Fernando Sanches <ferbs89@gmail.com>
2021-11-15 22:49:16 -0300

    Chapter II - Primeira aplicação web com ReactJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createServer, Model } from 'miragejs';
+import { App } from './App';
+
+createServer({
+	models: {
+		transaction: Model,
+	},
+
+	seeds(server) {
+		server.db.loadData({
+			transactions: [
+				{
+					id: 1,
+					title: 'Freelance de website',
+					amount: 6000,
+					type: 'deposit',
+					category: 'Dev',
+					createdAt: new Date('2021-11-13 13:18:00'),
+				},
+				{
+					id: 2,
+					title: 'Aluguel',
+					amount: 1100,
+					type: 'withdraw',
+					category: 'Casa',
+					createdAt: new Date('2021-11-16 13:19:00'),
+				},
+			],
+		});
+	},
+
+	routes() {
+		this.namespace = 'api';
+
+		this.get('/transactions', () => {
+			return this.schema.all('transaction');
+		});
+
+		this.post('/transactions', (schema, request) => {
+			const data = JSON.parse(request.requestBody);
+
+			return schema.create('transaction', data);
+		});
+	}
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
